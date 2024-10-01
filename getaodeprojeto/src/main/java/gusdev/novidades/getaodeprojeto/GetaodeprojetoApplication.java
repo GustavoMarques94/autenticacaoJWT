fc1bafc6b6236f3f52a70d187806cdd3c996b0d7
GustavoMarques94 <marques.lameirao@gmail.com>
2024-09-30 23:00:18 -0300

    Criação da estrutura do projeto de Autenticação JWT, juntamente com arquivos de configuração dockfiler, .gitignore, init.sql e application.yml

---
+++ getaodeprojeto/src/main/java/gusdev/novidades/getaodeprojeto/GetaodeprojetoApplication.java
@@ -0,0 +1,13 @@
+package gusdev.novidades.getaodeprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetaodeprojetoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetaodeprojetoApplication.class, args);
+    }
+
+}
---
+++ getaodeprojeto/src/test/java/gusdev/novidades/getaodeprojeto/GetaodeprojetoApplicationTests.java
@@ -0,0 +1,9 @@
+package gusdev.novidades.getaodeprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetaodeprojetoApplicationTests {
+
+}
